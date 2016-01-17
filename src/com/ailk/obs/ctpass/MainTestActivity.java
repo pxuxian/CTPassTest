ef290704144f387ddf58fabeee4e70c0cd1ceb74
panxuxian <panxuxian@youku.com>
2016-01-17 18:25:26 +0800

    INIT

--- src/com/ailk/obs/ctpass/MainTestActivity.java
+++ src/com/ailk/obs/ctpass/MainTestActivity.java
@@ -1,5 +1,8 @@
 package com.ailk.obs.ctpass;
 
+import com.ailk.obs.ctpass.activity.IndexActivity;
+import com.ailk.obs.ctpass.log.LogUtil;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +12,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.ailk.obs.ctpass.activity.IndexActivity;
-import com.ailk.obs.ctpass.log.LogUtil;
 
 public class MainTestActivity extends Activity implements OnClickListener {
 
