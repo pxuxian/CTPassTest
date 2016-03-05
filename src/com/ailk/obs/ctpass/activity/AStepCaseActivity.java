2a9e4500efa4b4368a2321250d7dd5aa2ec42b60
pxuxian <pxuxian@qq.com>
2016-03-05 17:21:16 +0800

    一键测试

--- src/com/ailk/obs/ctpass/activity/AStepCaseActivity.java
+++ src/com/ailk/obs/ctpass/activity/AStepCaseActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.RemoteException;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,15 +17,17 @@ import com.ailk.obs.ctpass.R;
 import com.ailk.obs.ctpass.constant.Constants;
 import com.ailk.obs.ctpass.manage.BindServiceConnection;
 import com.ailk.obs.ctpass.manage.BindServiceManager;
+import com.ailk.obs.ctpass.util.HandlerUtil;
 
 public class AStepCaseActivity extends Activity {
 	private CheckBox mButtonBindService;
+	private CheckBox mButtonConnect;
 	private String phoneNumber;
 	private String pcCode;
 	private int hostUrl;
 	private Button mButtonStepTest;
 
-	// TOKEN OTA认证，检查认证结果
+	// 检查认证结果
 	@SuppressLint("HandlerLeak")
 	private Handler handler = new Handler() {
 		@Override
@@ -39,6 +42,15 @@ public class AStepCaseActivity extends Activity {
 				} else {
 					mButtonBindService.setBackgroundColor(Constants.COLOR_RED);
 				}
+
+				break;
+			case Constants.CASE_CONN:
+				if (msg.getData().getBoolean("FLAG")) {
+					mButtonConnect.setBackgroundColor(Constants.COLOR_GREEN);
+				} else {
+					mButtonConnect.setBackgroundColor(Constants.COLOR_RED);
+				}
+				break;
 			default:
 				break;
 			}
@@ -56,6 +68,7 @@ public class AStepCaseActivity extends Activity {
 
 	private void initView() {
 		mButtonBindService = (CheckBox) findViewById(R.id.buttonBindService);
+		mButtonConnect = (CheckBox) findViewById(R.id.buttonConnect);
 		mButtonStepTest = (Button) findViewById(R.id.bt_aStepTest);
 
 		Intent intent = getIntent();
@@ -72,9 +85,24 @@ public class AStepCaseActivity extends Activity {
 		mButtonStepTest.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
+				// 绑定服务
 				final String bindServiceFlag = mButtonBindService.isChecked() == true ? "1" : "0";
 				if (bindServiceFlag.equals("1")) {
 					bindServiceManager.bindService(view.getContext(), serviceConnection, handler);
+				}
+
+				// 建立机卡连接
+				final String connectServiceFlag = mButtonConnect.isChecked() == true ? "1" : "0";
+				if (connectServiceFlag.equals("1")) {
+					if (serviceConnection.getCtpassAIDLService() != null) {
+						try {
+							serviceConnection.getCtpassAIDLService().connectCTPassService();
+							return;
+						} catch (RemoteException e) {
+							e.printStackTrace();
+						}
+					}
+					HandlerUtil.send(handler, Constants.CASE_CONN, "建立机卡连接失败", false);
 				}
 			}
 		});
